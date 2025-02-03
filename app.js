be7497027d3ba67b6c87bc1e41bdb166ff1ea1e9
Stine <stine.vik.letrud@gmail.com>
2025-02-03 16:24:55 +0000

    Setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files (CSS, JS, images)
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Daily Planner App!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
