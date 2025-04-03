d120636272987bf4f8c194a888bf8c815282f0a3
Inerud <s.letrud1@uni.brighton.ac.uk>
2025-04-03 12:13:34 +0200

    nav

---
+++ public/js/nav.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+const tools = document.getElementById("tools");
+
+    function displaymenu() {
+        tools.innerHTML = ""; // Clear existing
+
+        tools.innerHTML = `
+        <ul>
+            <li><a href="habits">Habits</a></li>
+            <hr>
+            <li><a href="mealplanner">Meal Planner</a></li>
+            <hr>
+            <li><a href="journal">Journal</a></li>
+            <hr>
+            <li><a href="todo">To-do</a></li>
+            <hr>
+            <li><a href="../">Calendar</a></li>
+            <hr>
+            <li><a href="../">Notes</a></li>
+            <hr>
+        </ul>
+        `
+        // <ul class="account">
+        //     <hr>
+        //     <li></span> <span id="auth-section"><a href="/login">login</a></span>
+        //     </li>
+        //     <hr>
+        // </ul>
+    };
+
+    displaymenu();
+
+    });
